740502c7fde0fb654107ed02a58179157487b54c
Jasmine Diaconu <jasmine.diaconu@yahoo.com>
2020-06-01 15:16:47 +0200

    Added HashTable implementation

---
+++ src/tests/exam/examAB/HashTableTest.java
@@ -0,0 +1,33 @@
+package exam.examAB;
+
+import exam.FinalB.HashTable.HashTable;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HashTableTest {
+    @Test
+    public void testLinearProbing() {
+        HashTable tab = new HashTable(1);
+        assertTrue(tab.put("apple", "juice"));
+        assertEquals("juice", tab.get("apple"));
+        assertEquals(true, tab.remove("apple"));
+        assertEquals(null, tab.table[0].getKey());
+        assertEquals(null, tab.table[0].getValue());
+        assertEquals(null, tab.get("apple"));
+    }
+
+    @Test
+    public void testIllegalArgument() {
+        try {
+            new HashTable(0);
+        } catch (IllegalArgumentException e1) {
+            try {
+                new HashTable(-42);
+            } catch (IllegalArgumentException e2) {
+                return;
+            }
+        }
+        fail();
+    }
+}
